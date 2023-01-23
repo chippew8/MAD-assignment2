0b95f6df0575268b6ce0def12c3c1ad78860c2ac
chippew8 <xuanxuan.apx@hotmail.com>
2023-01-23 14:31:35 +0800

    basic part 1 lol

--- MovieViewerBasic_Starter/app/src/main/java/nyp/sit/movieviewer/basic/SimpleViewListOfMoviesActivity.kt
+++ MovieViewerBasic_Starter/app/src/main/java/nyp/sit/movieviewer/basic/SimpleViewListOfMoviesActivity.kt
@@ -1,25 +1,31 @@
 package nyp.sit.movieviewer.basic
 
+import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
+import android.view.ViewGroup
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_view_list_of_movies.*
+import nyp.sit.movieviewer.basic.data.SimpleMovieSampleData
 import nyp.sit.movieviewer.basic.data.SimpleMovieSampleData.Companion.populateSimpleMovieItem
 import nyp.sit.movieviewer.basic.data.SimpleMovieSampleData.Companion.simpleMovieitemArray
-
+import nyp.sit.movieviewer.basic.entity.SimpleMovieItem
 
 
 class SimpleViewListOfMoviesActivity : AppCompatActivity() {
 
-    private val movies = populateSimpleMovieItem()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_list_of_movies)
 
-        val arrayMoviesAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_1, movies)
+//        val arrayMoviesAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_1)
+        var arrayMoviesAdapter = CustListAdapter(this, populateSimpleMovieItem())
         movielist.adapter = arrayMoviesAdapter
+
 
         movielist.onItemClickListener =object: AdapterView.OnItemClickListener{
 
@@ -28,13 +34,54 @@ class SimpleViewListOfMoviesActivity : AppCompatActivity() {
             }
         }
     }
+    class CustListAdapter(context: Context, data:ArrayList<SimpleMovieItem>):BaseAdapter() {
 
 
-}
+        internal val sList:ArrayList<SimpleMovieItem> = SimpleMovieSampleData.simpleMovieitemArray
 
-fun title_array(movies){
+        private val mInflater : LayoutInflater
+        init{
 
-    for(i in movies){
+            this.mInflater = LayoutInflater.from(context)
+            sList?.addAll(data)
+        }
 
+
+        fun addAll(data: Collection<SimpleMovieItem>){
+
+            sList?.addAll(data)
+
+        }
+        fun clear(){
+
+            sList?.clear()
+
+        }
+        override fun getItem(p0: Int): Any? {
+
+            return sList?.get(p0)
+        }
+
+        override fun getItemId(p0: Int): Long {
+
+            return 0
+        }
+
+        override fun getCount(): Int {
+            return if(sList == null) 0 else sList?.size
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View{
+            val v : View
+            v = this.mInflater.inflate(R.layout.simpleitem, parent, false)
+            val label: TextView = v.findViewById(R.id.movie_name)
+            label.text = sList?.get(position)?.title
+
+            return v
+        }
     }
+
+
+
 }
+
